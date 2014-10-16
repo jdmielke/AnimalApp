3824e6febc109f6ea4d40d7f10951496873c152d
jdmielke <jdmielke@iastate.edu>
2014-10-15 23:03:38 -0500

    Stuff that wasn't commited

---
+++ AWEA/src/com/exmaple/awea/test/HomeActivityTest.java
@@ -0,0 +1,24 @@
+package com.exmaple.awea.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HomeActivityTest {
+
+	HomeActivityTest (){
+		
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test2() {
+		String msg = "hey";
+		assertTrue(msg.equalsIgnoreCase("hey"));
+	}
+
+}
