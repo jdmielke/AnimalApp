68dccf771a0123064714bd4fd5b85ccb4e9efc90
jdmielke <jdmielke@iastate.edu>
2014-11-12 16:04:23 -0600

    data source update
    This is part of the previous commit

--- AWEA/src/com/example/db/DairyDataSource.java
+++ AWEA/src/com/example/db/DairyDataSource.java
@@ -11,70 +11,70 @@ import android.database.sqlite.SQLiteDatabase;
 
 public class DairyDataSource {
 
-  // Database fields
-  private SQLiteDatabase database;
-  private MySQLiteHelper dbHelper;
-  private String[] allColumns = { MySQLiteHelper.COLUMN_ID,
-      MySQLiteHelper.COLUMN_WEIGHT, MySQLiteHelper.COLUMN_LENGTH, MySQLiteHelper.COLUMN_GIRTH };
+	// Database fields
+	private SQLiteDatabase database;
+	private MySQLiteHelper dbHelper;
+	private String[] allColumns = { MySQLiteHelper.COLUMN_ID,
+			MySQLiteHelper.COLUMN_WEIGHT, MySQLiteHelper.COLUMN_LENGTH, MySQLiteHelper.COLUMN_GIRTH };
 
-  public DairyDataSource(Context context) {
-    dbHelper = new MySQLiteHelper(context);
-  }
+	public DairyDataSource(Context context) {
+		dbHelper = new MySQLiteHelper(context);
+	}
 
-  public void open() throws SQLException {
-    database = dbHelper.getWritableDatabase();
-  }
+	public void open() throws SQLException {
+		database = dbHelper.getWritableDatabase();
+	}
 
-  public void close() {
-    dbHelper.close();
-  }
+	public void close() {
+		dbHelper.close();
+	}
 
-  public Dairy createDairy(String weight, String length, String girth) {
-    ContentValues values = new ContentValues();
-    values.put(MySQLiteHelper.COLUMN_WEIGHT, weight);
-    values.put(MySQLiteHelper.COLUMN_LENGTH, length);
-    values.put(MySQLiteHelper.COLUMN_GIRTH, girth);
-    long insertId = database.insert(MySQLiteHelper.TABLE_DAIRY, null,
-        values);
-    Cursor cursor = database.query(MySQLiteHelper.TABLE_DAIRY,
-        allColumns, MySQLiteHelper.COLUMN_ID + " = " + insertId, null,
-        null, null, null);
-    cursor.moveToFirst();
-    Dairy newDairy = cursorToDairy(cursor);
-    cursor.close();
-    return newDairy;
-  }
+	public Dairy createDairy(String weight, String length, String girth) {
+		ContentValues values = new ContentValues();
+		values.put(MySQLiteHelper.COLUMN_WEIGHT, weight);
+		values.put(MySQLiteHelper.COLUMN_LENGTH, length);
+		values.put(MySQLiteHelper.COLUMN_GIRTH, girth);
+		long insertId = database.insert(MySQLiteHelper.TABLE_DAIRY, null,
+				values);
+		Cursor cursor = database.query(MySQLiteHelper.TABLE_DAIRY,
+				allColumns, MySQLiteHelper.COLUMN_ID + " = " + insertId, null,
+				null, null, null);
+		cursor.moveToFirst();
+		Dairy newDairy = cursorToDairy(cursor);
+		cursor.close();
+		return newDairy;
+	}
 
-  public void deleteComment(Dairy dairy) {
-    long id = dairy.getId();
-    System.out.println("Comment deleted with id: " + id);
-    database.delete(MySQLiteHelper.TABLE_DAIRY, MySQLiteHelper.COLUMN_ID
-        + " = " + id, null);
-  }
+	public void deleteComment(Dairy dairy) {
+		long id = dairy.getId();
+		System.out.println("Comment deleted with id: " + id);
+		database.delete(MySQLiteHelper.TABLE_DAIRY, MySQLiteHelper.COLUMN_ID
+				+ " = " + id, null);
+	}
 
-  public List<Dairy> getAllComments() {
-    List<Dairy> dairys = new ArrayList<Dairy>();
+	public List<Dairy> getAllComments() {
+		List<Dairy> dairys = new ArrayList<Dairy>();
 
-    Cursor cursor = database.query(MySQLiteHelper.TABLE_DAIRY,
-        allColumns, null, null, null, null, null);
+		Cursor cursor = database.query(MySQLiteHelper.TABLE_DAIRY,
+				allColumns, null, null, null, null, null);
 
-    cursor.moveToFirst();
-    while (!cursor.isAfterLast()) {
-      Dairy dairy = cursorToDairy(cursor);
-      dairys.add(dairy);
-      cursor.moveToNext();
-    }
-    // make sure to close the cursor
-    cursor.close();
-    return dairys;
-  }
+		cursor.moveToFirst();
+		while (!cursor.isAfterLast()) {
+			Dairy dairy = cursorToDairy(cursor);
+			dairys.add(dairy);
+			cursor.moveToNext();
+		}
+		// make sure to close the cursor
+		cursor.close();
+		return dairys;
+	}
 
-  private Dairy cursorToDairy(Cursor cursor) {
-    Dairy dairy = new Dairy();
-    dairy.setId(cursor.getLong(0));
-    dairy.setWeight(cursor.getString(1));
-    dairy.setLength(cursor.getString(2));
-    dairy.setGirth(cursor.getString(3));
-    return dairy;
-  }
+	private Dairy cursorToDairy(Cursor cursor) {
+		Dairy dairy = new Dairy();
+		dairy.setId(cursor.getLong(0));
+		dairy.setWeight(cursor.getString(1));
+		dairy.setLength(cursor.getString(2));
+		dairy.setGirth(cursor.getString(3));
+		return dairy;
+	}
 } 
